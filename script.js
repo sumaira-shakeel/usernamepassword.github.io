16fb9846324d40488fb68c17a7171cfadf5d6ea0
unknown <sumairasami2809@gmail.com>
2024-07-03 00:41:14 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+
+const userName = prompt("Enter your name: ");
+const userPassword = prompt("Enter your password: ");
+
+if (userName === "Bilal" && userPassword === "BMJ") {
+  alert("Login successful!");
+} else {
+  alert("Invalid username or password!");
+}
